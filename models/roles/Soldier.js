f5df6d4d746f62347f9a16618882b4c2a0125152
周勇 <1059981087@qq.com>
2015-01-15 10:15:03 +0800

    Using exhaustive enumeration method to enumerate all the possibilities. Question three complete

--- models/roles/Soldier.js
+++ models/roles/Soldier.js
@@ -13,21 +13,23 @@ Soldier.prototype = Object.create(People.prototype);
 Soldier.prototype.constructor = Soldier;
 
 Soldier.prototype.out_hurt = function() {
-    return this.hurt + this.weapon.hurt;
+    if(this.weapon) {
+        return this.hurt + this.weapon.hurt;
+    }
+    return this.hurt;
 };
 
 Soldier.prototype.get_hurt = function(hurt) {
-    var _hurt = hurt - this.defense.value;
+    var _hurt = this.defense?hurt - this.defense.value:hurt;
     return _hurt>0 ? _hurt : 0;
 };
 
 Soldier.prototype.reduce_blood = function(hurt) {
-    var _hurt = hurt - this.defense.value;
-    this.blood -= _hurt>0 ? _hurt : 0;
+    this.blood -= this.get_hurt(hurt);
 };
 
 Soldier.prototype.output_fight_details = function(enemy) {
-    return this.role + this.name + '用' + this.weapon.name + '攻击了' +
+    return this.role + this.name + (this.weapon?'用'+this.weapon.name:'') + '攻击了' +
         enemy.role + enemy.name + ',' + enemy.name + '受到了' +
         enemy.get_hurt(this.out_hurt()) + '点伤害,' + enemy.name +
         '剩余生命:' + enemy.blood + '\n';
--- spec/player_spec.js
+++ spec/player_spec.js
@@ -85,6 +85,86 @@ describe("weapon-evolution", function(){
         })
     });
 
+    describe('Test the fight method of person.',function() {
+
+        describe('Soldier fight ordinary being.',function() {
+
+            it('Soldier have weapon.',function() {
+                var soldier = new Soldier('张三', 10, 3, '战士');
+                soldier.weapon = new Weapon('优质木棒', 2);
+                var ordinaryBeing = new People('李四', 10, 2, '普通人');
+                expect(soldier.fight(ordinaryBeing))
+                    .toBe('战士张三用优质木棒攻击了普通人李四,李四受到了5点伤害,李四剩余生命:5\n');
+            });
+
+            it('Soldier do not have weapon.',function() {
+                var soldier = new Soldier('张三', 10, 3, '战士');
+                var ordinaryBeing = new People('李四', 10, 2, '普通人');
+                expect(soldier.fight(ordinaryBeing))
+                    .toBe('战士张三攻击了普通人李四,李四受到了3点伤害,李四剩余生命:7\n');
+            });
+       });
+
+        describe('Ordinary being fight soldier.',function() {
+
+            it('Soldier have defense.',function() {
+                var soldier = new Soldier('张三', 10, 5, '战士');
+                soldier.defense = new Defense('盾牌', 2);
+                var ordinaryBeing = new People('李四', 10, 4, '普通人');
+                expect(ordinaryBeing.fight(soldier))
+                    .toBe('普通人李四攻击了战士张三,张三受到了2点伤害,张三剩余生命:8\n');
+            });
+
+            it('Soldier do not have defense.',function() {
+                var soldier = new Soldier('张三', 10, 5, '战士');
+                var ordinaryBeing = new People('李四', 10, 4, '普通人');
+                expect(ordinaryBeing.fight(soldier))
+                    .toBe('普通人李四攻击了战士张三,张三受到了4点伤害,张三剩余生命:6\n');
+            });
+        });
+
+        describe('Soldier fight soldier.',function() {
+
+            it('Soldier one have weapon and soldier two do not have defense.',function() {
+                var soldierOne = new Soldier('张三', 10, 5, '战士');
+                soldierOne.weapon = new Weapon('优质木棒', 3);
+                var soldierTwo = new Soldier('李四', 10, 4, '战士');
+                expect(soldierOne.fight(soldierTwo))
+                    .toBe('战士张三用优质木棒攻击了战士李四,李四受到了8点伤害,李四剩余生命:2\n');
+            });
+
+            it('Soldier one have weapon and soldier two have defense.',function() {
+                var soldierOne = new Soldier('张三', 10, 5, '战士');
+                soldierOne.weapon = new Weapon('优质木棒', 3);
+                var soldierTwo = new Soldier('李四', 10, 4, '战士');
+                soldierTwo.defense = new Defense('盾牌', 2);
+                expect(soldierOne.fight(soldierTwo))
+                    .toBe('战士张三用优质木棒攻击了战士李四,李四受到了6点伤害,李四剩余生命:4\n');
+            });
+
+            it('Soldier one do not have weapon and soldier two do not have defense.',function() {
+                var soldierOne = new Soldier('张三', 10, 5, '战士');
+                var soldierTwo = new Soldier('李四', 10, 4, '战士');
+                expect(soldierOne.fight(soldierTwo))
+                    .toBe('战士张三攻击了战士李四,李四受到了5点伤害,李四剩余生命:5\n');
+            });
+
+            it('Soldier one do not have weapon and soldier two have defense.',function() {
+                var soldierOne = new Soldier('张三', 10, 5, '战士');
+                var soldierTwo = new Soldier('李四', 10, 4, '战士');
+                soldierTwo.defense = new Defense('盾牌', 2);
+                expect(soldierOne.fight(soldierTwo))
+                    .toBe('战士张三攻击了战士李四,李四受到了3点伤害,李四剩余生命:7\n');
+            });
+        });
+
+        it('Ordinary being fight ordinary being.',function() {
+            var ordinary1 = new People('张三', 10, 3, '普通人');
+            var ordinary2 = new People('李四', 10, 2, '普通人');
+            expect(ordinary1.fight(ordinary2))
+                .toBe('普通人张三攻击了普通人李四,李四受到了3点伤害,李四剩余生命:7\n'); });
+    });
+
     it('Should output details of the fighting.',function() {
         var soldier = new Soldier('张三', 10, 4,'战士',
             new Weapon('优质木棒',3),
